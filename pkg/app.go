4d9d4309869beea95185003a1a775eb83b391788
cuisongliu <cuisongliu@qq.com>
2019-09-10 21:02:32 +0800

    fix json

--- pkg/app.go
+++ pkg/app.go
@@ -1,7 +1,21 @@
 package pkg
 
-import "github.com/wonderivan/logger"
+import (
+	"encoding/json"
+	"github.com/fanux/sealbuild/pkg/utils"
+	"github.com/wonderivan/logger"
+	"io/ioutil"
+)
 
 func app() {
-	logger.Warn("app")
+	//生成的文件名称
+	fileName := utils.VarsConfig.AppName + utils.VarsConfig.AppVersion + ".tar.gz"
+	logger.Warn("app:%s", fileName)
+	imagesData, _ := ioutil.ReadFile(utils.VarsConfig.AppImages)
+	logger.Debug("\njson:%s", string(imagesData))
+	images := []string{}
+	_ = json.Unmarshal(imagesData, images)
+	//for _,v:=range images.image {
+	//	logger.Info("images:%s",v)
+	//}
 }
