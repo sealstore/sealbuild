a23ffda5cfb0571f0a4efff0281b1b47402474e7
cuisongliu <cuisongliu@qq.com>
2019-09-12 00:29:31 +0800

    fix docker pull use go client

--- pkg/app.go
+++ pkg/app.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/fanux/sealbuild/pkg/utils"
 	"github.com/wonderivan/logger"
 	"io/ioutil"
@@ -13,9 +14,19 @@ func app() {
 	logger.Warn("app:%s", fileName)
 	imagesData, _ := ioutil.ReadFile(utils.VarsConfig.AppImages)
 	logger.Debug("\njson:%s", string(imagesData))
-	images := []string{}
-	_ = json.Unmarshal(imagesData, images)
-	//for _,v:=range images.image {
-	//	logger.Info("images:%s",v)
-	//}
+	var images []string
+	//注意数组转换需要加 &
+	_ = json.Unmarshal(imagesData, &images)
+	for _, v := range images {
+		if v != "" {
+			logger.Info("images:%s", v)
+			cmd := fmt.Sprintf("docker pull %s", v)
+			var command string
+			var err error
+			if command, err = utils.Shell(cmd); err != nil {
+				logger.Error(err)
+			}
+			logger.Alert(command)
+		}
+	}
 }
